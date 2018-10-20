ce37b6e487bcdac8fe95b3df81afe7c590716e53
suxin1 <suxin1@live.com>
2018-10-20 09:52:37 +0800

    update sineWave/main.js

--- apps/public/pages/sineWave/main.js
+++ apps/public/pages/sineWave/main.js
@@ -11,16 +11,13 @@ import canvas_obj from "./canvas";
  * @returns {Function}
  * @constructor
  */
-const sine = function (a, b, c, d) {
-  return function (t) {
+const sine = function (a, b, c, d, t) {
     return a * Math.sin(b * t + c) + d;
-  }
 };
 
 
 const singleSineWave = {
   init: function () {
-    this.sine_wave_function = sine(20, 5, 0, 0);
     let width = 300,
       height = 150;
 
@@ -32,13 +29,11 @@ const singleSineWave = {
     // Fix blur on mobile
     set.attr.height(get.style.height() * dpi);
     set.attr.width(get.style.width() * dpi);
+    ctx.scale(dpi, dpi);
 
     this.ctx = ctx;
     this.width = width;
     this.height = height;
-
-    this.draw();
-
 
     if (window.requestAnimationFrame) {
       let animation = () => {
@@ -62,36 +57,85 @@ const singleSineWave = {
 
     // Set background color
     ctx.fillStyle = "rgba(10, 20, 60)";
-    ctx.rect(0, 0, width, height);
-    ctx.fill();
+    ctx.fillRect(0, 0, width, height);
 
-    // Draw Sine Wave
+    // Draw Sine Wave 1
     ctx.save();
-    this.drawSine(0);
-    ctx.stroke();
+    ctx.translate(0, height / 2);
+    this.drawSine(t);
+
+    // Draw Sine Wave 2
+
     ctx.restore();
   },
 
   drawSine: function (t) {
     const {ctx, width, height} = this;
+    let amp_cache = [];
+
     let strokeStyle = ctx.createLinearGradient(0, 0, width, height);
     strokeStyle.addColorStop(0, "red");
     strokeStyle.addColorStop(1, "green");
+
+    let greenToYellow = ctx.createLinearGradient(0, 0, width, height);
+    greenToYellow.addColorStop(0, "green");
+    greenToYellow.addColorStop(1, "yellow");
+
     ctx.strokeStyle = strokeStyle;
 
-    // Set the initial x and y.
-    let x = t,
-      y = this.sine_wave_function(this.generalize(x));
+    // pos = v * t;
+    // 1 pix/s = 1pix / 1000 ms;
+    // pos = 1pix / 1000ms * time;
+    let pos = t / 1000;
 
-    ctx.save();
-    ctx.translate(0, height / 2);
+    // pos2 = v2 * t; v2 = 2 pix/s = 2pix / 1000ms;
+    let pos2 = t / 500;
+
+    // Set the initial x and y.
+    let scale = (Math.sin(-Math.PI * .5) + 1) * .5;
+    let x = t,
+      y = sine(40, 4, -pos, 0, this.generalize(0)) * scale;
+
     ctx.beginPath();
 
     ctx.moveTo(0, y);
     for (let x = 0; x < width; x++) {
-      let y = this.sine_wave_function(this.generalize(x));
-      ctx.lineTo(x, y);
+      // Map x to 0 and 2Pi
+      let radians = x / width * Math.PI  * 2;
+      let scale = (Math.sin(radians - Math.PI * .5) + 1) * .5;
+      amp_cache.push(scale);
+
+      let y = sine(40, 4, -pos, 0, this.generalize(x));
+      ctx.lineTo(x, y * scale);
     }
+    ctx.stroke();
+
+    ctx.moveTo(x, y);
+    for (let x = 0; x < width; x++) {
+      // Map x to 0 and 2Pi
+      let radians = x / width * Math.PI  * 2;
+      let scale = (Math.sin(radians - Math.PI * .5) + 1) * .5;
+      amp_cache.push(scale);
+
+      let y = sine(40, 4, pos, 0, this.generalize(x));
+      ctx.lineTo(x, y * scale);
+    }
+    ctx.stroke();
+
+    ctx.strokeStyle = greenToYellow;
+    ctx.beginPath();
+    ctx.moveTo(0, amp_cache[0]);
+    for(let x =0;x<width;x++) {
+      ctx.lineTo(x, -amp_cache[x]*40);
+    }
+    ctx.stroke();
+
+    // ctx.beginPath();
+    // ctx.moveTo(0, amp_cache[0]);
+    // for(let x =0;x<width;x++) {
+    //   ctx.lineTo(x, amp_cache[x]*40);
+    // }
+    // ctx.stroke();
   }
 };
 
