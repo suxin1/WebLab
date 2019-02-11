6b361948809227e61b80a414ef1d54c0839418aa
何书鑫 <960561464@qq.com>
2019-02-11 18:35:26 +0800

    optimize water_fall

--- apps/source/pages/water_fall/main.js
+++ apps/source/pages/water_fall/main.js
@@ -1,8 +1,8 @@
 import './styles.scss';
 
+import {fastSolve} from "./partition";
+
 const IMAGE_BASE_URL = 'https://picsum.photos';
-const MAINCONTENT_WIDTH = 1200;
-const IMAGE_COLUMNS = 4;
 
 function getImages() {
   return fetch('https://picsum.photos/list', {method: "GET"}).then(response => {
@@ -18,12 +18,11 @@ function getImages() {
  * @returns {HTMLElement}
  */
 function createImage(data, width=1200) {
-  let static_width = width / 4 - 10;
   let container = document.createElement('div');
   container.classList.add('image_container');
   let image = document.createElement('img');
-  image.style.width = `${static_width}px`;
-  image.style.height = `${data.height * static_width / data.width}px`;
+  image.style.width = `${data.width - 10}px`;
+  image.style.height = `${data.height}px`;
   image.style.background = "#aaa";
   image.src = `${IMAGE_BASE_URL}/${data.width}/${data.height}/?image=${data.id}`;
   container.appendChild(image);
@@ -46,6 +45,13 @@ function sum(list, callback) {
   }, 0);
 }
 
+
+/**
+ * The greedy algorithm use to partition a list
+ * @param list
+ * @param n
+ * @returns {any[]}
+ */
 function find_partition_greedy(list, n = 3) {
   let sortedList = list.sort((a, b) => {
     return a.height - b.height;
@@ -75,22 +81,60 @@ function find_partition_greedy(list, n = 3) {
   return sets;
 }
 
+
+function find_partition_dynamic(items, n=3) {
+  let sum = items.reduce((s, item) => {
+    return s + item.height;
+  }, 0);
+
+  let base = Math.floor(sum/n);
+
+  let res = [];
+  let solved = [];
+
+  for(let i=0;i<n;i++) {
+    let remains = items.filter(item => {
+      return !solved.some(i => i.id === item.id);
+    });
+    let partial = fastSolve(remains, base);
+
+    solved = [...solved, ...partial[1]];
+    res.push(partial);
+  }
+  return res.map(i => i[1]);
+}
+
+
+// water fall controller
 let water_fall = (function() {
   return {
     container: document.querySelector('#image_collection'),
+    container_width: 1200,
+    columns: 4,
     image_containers: [],
-    init: function(list, length) {
-      for(let i=0;i<IMAGE_COLUMNS;i++) {
+    init: function(list) {
+      for(let i=0;i<this.columns;i++) {
         let column = document.createElement('div');
         column.style.float = "left";
-        column.style.width = `${(1/4 * 100).toFixed(2)}%`;
+        column.style.width = `${(1/this.columns * 100).toFixed(2)}%`;
         this.image_containers.push(column);
         this.container.appendChild(column);
       }
 
-      let initial_collection = find_partition_greedy(list, length);
-
+      let newList = this.imageDataProcess(list);
+      // let initial_collection = find_partition_greedy(newList, length);
+      let initial_collection = find_partition_dynamic(newList, this.columns);
       this.addImage(initial_collection);
+    },
+    imageDataProcess: function(jsonData) {
+      let newData = jsonData.map((image) => {
+        let width = Math.floor(this.container_width/this.columns);
+        let height = Math.floor(image.height * width / image.width + Math.random()*100);
+        image.width = width;
+        image.height = height;
+        return image;
+      });
+      return newData;
     },
     addImage: function(collection) {
       let maxLength = collection[0].length;
@@ -98,7 +142,6 @@ let water_fall = (function() {
       for(let i=0;i<collection.length;i++) {
         if(collection[i].length>maxLength) maxLength=collection[i].length;
       }
-
       // append image from top to bottom
       for(let i=0;i<maxLength;i++) {
         for(let j=0;j<collection.length;j++) {
@@ -112,5 +155,5 @@ let water_fall = (function() {
 })();
 
 getImages().then(jsonData => {
-  water_fall.init(jsonData.splice(200, 30), IMAGE_COLUMNS)
+  water_fall.init(jsonData.splice(154, 300))
 });
---
+++ apps/source/pages/water_fall/partition.js
@@ -0,0 +1,31 @@
+export function fastSolve(items, avail, memo=null) {
+
+  let result = [];
+  if(!memo) memo = {};
+
+  if(items.length === 0 || avail === 0) {
+    result = [0, []];
+  }
+  else if(items[0].height > avail) {
+    result = fastSolve(items.splice(1), avail);
+  }
+  else {
+    let current_item = items[0];
+
+    // Explore left branch
+    let [withValue, withItems] = fastSolve(items.splice(1), avail - current_item.height);
+    withValue += current_item.height;
+    withItems.push(current_item);
+
+    // Explore right branch
+    let [withoutValue, withoutItems] = fastSolve(items.splice(1), avail);
+
+    if(withValue > withoutValue) {
+      result = [withValue, withItems];
+    } else {
+      result = [withoutValue, withoutItems];
+    }
+  }
+
+  return result;
+}
